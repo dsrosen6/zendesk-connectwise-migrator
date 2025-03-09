acb34c24127b5e81792c2e2d4a3162e53d08e8ab
tctgdanny <154454046+tctgdanny@users.noreply.github.com>
2025-03-09 16:43:42 -0500

    begin tui build

---
+++ tui/tui.go
@@ -0,0 +1,20 @@
+package tui
+
+import (
+	"github.com/charmbracelet/bubbletea"
+)
+
+type MainModel struct {
+}
+
+func (m MainModel) Init() tea.Cmd {
+	return nil
+}
+
+func (m MainModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+
+}
+
+func (m MainModel) View() string {
+
+}
